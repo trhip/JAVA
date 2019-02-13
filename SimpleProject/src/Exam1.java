32fcd185164129ced5a24ec2d4f03e03a0bbae8f
cbnu <cbnu@1PC-14>
2019-02-13 15:44:40 +0900

    Initial commit

---
+++ SimpleProject/src/Exam1.java
@@ -0,0 +1,13 @@
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int SUM, A,B;
+		A=10;
+		B=25;
+		SUM=A+B;
+		System.out.println(SUM);
+	}
+
+}
