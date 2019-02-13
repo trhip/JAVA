c5c0485c33795e9ec834d68d912f13cd637e61e4
wjdqhcjfl <47588053+wjdqhcjfl@users.noreply.github.com>
2019-02-13 15:58:08 +0900

    Exam3

---
+++ SimpleProject/src/Exam2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int SUM = 0, N1, N2, N;
+		Scanner StdInupt = new Scanner(System.in);
+		N1 = StdInupt.nextInt();
+		N2 = StdInupt.nextInt();
+		for (N = N1; N <= N2; N++) {
+			SUM = SUM + N;
+		}
+		System.out.println(N1 + "부터 " + N2 + "까지의");
+		System.out.println("정수의 합은 " + SUM + "이다");
+	}
+
+}
---
+++ SimpleProject/src/Exam3.java
@@ -0,0 +1,16 @@
+
+public class Exam3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int NUM[] = { 70, 60, 50, 80, 90 };
+		int MIN = 9999, N;
+		for (N = 0; N < NUM.length; N++) {
+			if (MIN > NUM[N]) {
+				MIN = NUM[N];
+			}
+		}
+		System.out.println("배열 NUM의 원소들 중에서 최솟값은");
+		System.out.println(MIN + " 입니다");
+	}
+}
