6efe77c23b7d68cbb260510053c2bbad11227f91
wjdqhcjfl <47588053+wjdqhcjfl@users.noreply.github.com>
2019-02-14 16:15:05 +0900

    Practice Chap04_Section01_Row_Major

---
+++ SimpleProject/src/Chap04_Section01_Row_Major.java
@@ -0,0 +1,27 @@
+
+public class Chap04_Section01_Row_Major {
+
+	public static void main(String[] args) {
+		int A[][] = new int[5][5];
+		int V = 0;
+		int C = 0;
+		do {
+			int R = 0;
+			do {
+				V++;
+				A[R][C] = V;
+				R++;
+			} while (R <= 4);
+			C = C + 1;
+		} while (C <= 4);
+
+		for (int R = 0; R < 5; R++) {	// 배열 A[R][C] 출력
+			for (C = 0; C < 5; C++)
+				System.out.print(A[C][R] + "\t");
+
+			System.out.println();
+		}
+
+	}
+
+}
